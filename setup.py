a4300b68876a34e70f367f902917e82f2c6c26c3
Peder Toftegaard Olsen <pdr@trifork.com>
2013-09-27 10:46:19 +0200

    Fixed license. Bumped to v0.1.2.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='pyx509',
-    version='0.1',
+    version='0.1.2',
     packages=['x509','x509.pkcs7','x509.pkcs7.asn1_models',],
-    #license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='GNU Library General Public License',
     long_description=open('README').read(),
     install_requires=[
             "pyasn1 >= 0.1.7",
