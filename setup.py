27d293e165180734d271b9a74ad6288954fa636f
Peder Toftegaard Olsen <pdr@trifork.com>
2013-10-31 14:39:56 +0100

    Cleaned up setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='pyx509',
-    version='0.1.2',
-    packages=['x509','x509.pkcs7','x509.pkcs7.asn1_models',],
+    version='0.1.3',
+    packages=find_packages(),
     license='GNU Library General Public License',
     long_description=open('README').read(),
     install_requires=[
             "pyasn1 >= 0.1.7",
         ],
+    zip_safe=False,
 )
