5f2d87074fdbc6785a4115ac38685a8c58a289ef
Dusan Klinec <dusan.klinec@gmail.com>
2017-08-27 19:43:52 +0200

    new tests added

--- pyx509/test/test_simple.py
+++ pyx509/test/test_simple.py
@@ -28,11 +28,16 @@ import sys
 import unittest
 
 from pyx509 import commands
+from pyx509.models import PKCS7, PKCS7_SignedData
 
 TEST_DATA_DIR = join(os.path.dirname(__file__), 'data')
 TEST_CERTIFICATE = join(TEST_DATA_DIR, 'test_certificate.der')
 TEST_CERTIFICATE_TXT = join(TEST_DATA_DIR, 'test_certificate.txt')
 TEST_SIGNATURE = join(TEST_DATA_DIR, 'test_signature.der')
+TEST_SIGNATURE_3 = join(TEST_DATA_DIR, 'test_sig3.der')
+TEST_SIGNATURE_3_TXT = join(TEST_DATA_DIR, 'test_sig3.txt')
+TEST_SIGNATURE_4 = join(TEST_DATA_DIR, 'test_sig4.der')
+TEST_SIGNATURE_4_TXT = join(TEST_DATA_DIR, 'test_sig4.txt')
 TEST_SIGNATURE_TXT = join(TEST_DATA_DIR, 'test_signature.txt')
 TEST_TIMESTAMP = join(TEST_DATA_DIR, 'test_timestamp.der')
 TEST_TIMESTAMP_TXT = join(TEST_DATA_DIR, 'test_timestamp.txt')
@@ -60,21 +65,60 @@ class SimpleTest(unittest.TestCase):
         self.assertEqual(txt1, txt2)
 
     def test_timestamp(self):
+        # import sys
+        # sys.stdout = sys.__stdout__
         commands.print_signature_info(open(TEST_TIMESTAMP, 'rb').read())
-        txt1 = u'%s' % (sys.stdout.getvalue()).decode('utf-8')
-        txt2 = open(TEST_TIMESTAMP_TXT).read()
+
+        self.maxDiff = None
+        txt1 = sys.stdout.getvalue().decode('utf8')
+        txt2 = open(TEST_TIMESTAMP_TXT, 'rU', encoding='utf8').read()
+
+        # TODO: fix unicode
+        # pyasn1: char.py - the ._value is already broken unicode.
+        # UTF8String initialized, using prettyIn(), calling unicode on the input value
+        # which is OctetString, containing string encoded input, in encoding iso-8859-1
+        #
+        # unicode(x) call on the OctetString causes .decode('iso-8859-1') call which breaks
+        # the encoding.
+        # https://github.com/etingof/pyasn1/issues/65
+        #
+
+        # with open('/tmp/b1', 'w') as fh:
+        #     fh.write(txt1)
+        # with open('/tmp/b2', 'w') as fh:
+        #     fh.write(txt2)
         self.assertEqual(txt1, txt2)
 
     def test_timestamp_info(self):
         commands.print_timestamp_info(open(TEST_TIMESTAMP, 'rb').read())
         txt1 = u'%s' % sys.stdout.getvalue()
         txt2 = open(TEST_TIMESTAMP_INFO_TXT).read()
-
-        with open('/tmp/b1', 'w') as fh:
-            fh.write(txt1)
-        with open('/tmp/b2', 'w') as fh:
-            fh.write(txt2)
         self.assertEqual(txt1, txt2)
+
+    def test_signature_3(self):
+        self.der_test(TEST_SIGNATURE_3, TEST_SIGNATURE_3_TXT)
+
+    def test_signature_4(self):
+        self.der_test(TEST_SIGNATURE_4, TEST_SIGNATURE_4_TXT)
+
+    def der_test(self, fname, exam):
+        p7 = PKCS7.from_der(open(fname, 'rb').read())
+        self.sub_test(p7)
+        txt1 = sys.stdout.getvalue().decode('utf8')
+        txt2 = open(exam, 'rU', encoding='utf8').read()
+        self.assertEqual(txt1, txt2)
+
+    def sub_test(self, p7):
+        signed_date, valid_from, valid_to, signer = p7.get_timestamp_info()
+        print('Sign date: %s' % signed_date)
+        print('Sign not before: %s' % valid_from)
+        print('Sign not after: %s' % valid_to)
+        print('Signer: %s' % str(signer))
+        if len(p7.content.signerInfos) > 0:
+            signer_info = p7.content.signerInfos[0]
+            print('Signer serial: %s' % signer_info.serial_number)
+            print('Sign issuer: %s' % signer_info.issuer)
+            print('Sign algorithm: %s' % signer_info.oid2name(signer_info.digest_algorithm))
 
 
 if __name__ == '__main__':
